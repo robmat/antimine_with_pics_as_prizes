79e80b7de946a3f07c47ee8ea2ea8ea7ebb750dd
Bator <robbator@gmail.com>
2023-09-18 07:47:52 +0200

    feat: own ad ids

--- external/src/main/java/dev/lucasnlm/external/Ads.kt
+++ external/src/main/java/dev/lucasnlm/external/Ads.kt
@@ -1,10 +1,10 @@
 package dev.lucasnlm.external
 
 object Ads {
-    const val REWARD_AD = "ca-app-pub-3940256099942544/5224354917"
-    const val SECOND_REWARD_AD = "ca-app-pub-3940256099942544/5224354917"
-    const val INTERSTITIAL_AD = "ca-app-pub-3940256099942544/1033173712"
-    const val SECOND_INTERSTITIAL_AD = "ca-app-pub-3940256099942544/1033173712"
-    const val BANNER_AD = "ca-app-pub-3940256099942544/6300978111"
+    const val REWARD_AD = "ca-app-pub-9667420067790140/9945957646"
+    const val SECOND_REWARD_AD = "ca-app-pub-9667420067790140/1224077675"
+    const val INTERSTITIAL_AD = "ca-app-pub-9667420067790140/9102567695"
+    const val SECOND_INTERSTITIAL_AD = "ca-app-pub-9667420067790140/7789486025"
+    const val BANNER_AD = "ca-app-pub-9667420067790140/2728731036"
     const val MIN_FREQUENCY = 60 * 1000L
 }
