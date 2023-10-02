0f641126a926fa13c56b8fdacd0d949e64dfe083
Bator <robbator@gmail.com>
2023-10-02 14:50:48 +0200

    feat: no crash reporting

--- proprietary/src/main/java/dev/lucasnlm/external/CrashReporterImpl.kt
+++ proprietary/src/main/java/dev/lucasnlm/external/CrashReporterImpl.kt
@@ -1,9 +1,7 @@
 package dev.lucasnlm.external
 
-import com.google.firebase.crashlytics.FirebaseCrashlytics
-
 class CrashReporterImpl : CrashReporter {
     override fun sendError(message: String) {
-        FirebaseCrashlytics.getInstance().recordException(Exception(message))
+        // FOSS build doesn't log errors.
     }
 }
